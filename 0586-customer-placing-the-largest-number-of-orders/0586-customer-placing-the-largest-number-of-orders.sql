e828bd6ad7db7af83005b209495a21cb21ead46c
Mohit Dhote <89855871+mohit-dhote@users.noreply.github.com>
2026-04-04 19:33:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -2,8 +2,10 @@
 /* select count(*) as customer_number from orders where customer_number = (select max(customer_number) from orders o2 where count(*) > 1 );
 */
 
-select customer_number
-from orders
+select customer_number 
+from Orders 
 group by customer_number
-order by count(*) desc
-limit 1 ;
+order by count(customer_number) desc
+limit 1;
+
+
