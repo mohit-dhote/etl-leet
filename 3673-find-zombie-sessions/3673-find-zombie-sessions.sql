b9d1e0e0c0335ceb4be1eacde6cb48a74e7d9bf8
Mohit Dhote <89855871+mohit-dhote@users.noreply.github.com>
2026-06-06 22:37:20 +0530

    Time: 336 ms (44.18%), Space: 0B (100%) - LeetHub

---
+++ 3673-find-zombie-sessions/3673-find-zombie-sessions.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+select 
+    session_id, user_id, 
+    timestampdiff(minute, min(event_timestamp), max(event_timestamp))
+    as session_duration_minutes,
+    sum(case when event_type='scroll' then 1 else 0 end) as scroll_count
+from app_events
+group by session_id, user_id
+having
+    timestampdiff(minute, min(event_timestamp), max(event_timestamp)) > 30 and
+    sum(case when event_type='scroll' then 1 else 0 end) >= 5 and
+    sum(case when event_type='click' then 1 else 0 end)/sum(case when event_type='scroll' then 1 else 0 end)<0.20 and
+    sum(case when event_type='purchase' then 1 else 0 end) = 0
+
+order by scroll_count desc, session_id
